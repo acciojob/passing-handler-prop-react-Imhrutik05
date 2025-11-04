819dfb76ac8e3534bf9879163b078722da1f43c3
Imhrutik05 <hrutikchaudhari5@gmail.com>
2025-11-04 17:17:35 +0530

    Task Completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,20 @@ import '../styles/App.css';
 import Selection from './Selection';
 import ColourSelector from './ColourSelector';
 
-const colourConfig = [{
+const colourConfig = [
+  {
     key: 'blue',
     label: 'Blue',
     classname: 'btn-blue',
     background: 'rgb(34, 193, 195)'
-  }, {
+  },
+  {
     key: 'orange',
     label: 'Orange',
     classname: 'btn-orange',
     background: 'rgb(221, 112, 18)'
-  }, {
+  },
+  {
     key: 'green',
     label: 'Green',
     classname: 'btn-green',
@@ -24,14 +27,15 @@ const colourConfig = [{
 const title = 'Select the gradient and then the Box to change the color';
 
 const App = () => {
-  let [nextBackground, selectNextBackground] = useState({ background: "" })
+  let [nextBackground, selectNextBackground] = useState({ background: "" });
+
   const applyColor = (updateSelectionStyle) => {
     updateSelectionStyle(nextBackground)
   }
-
+  
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
         {colourConfig.map((config, index) => (
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,25 @@
+import React, { useState } from "react";
+
+function Selection({applyColor}){
+    const [selectionStyle, setSelectionStyle] = useState({background: "" });
+
+    return (
+        <div
+            className="fix-box"
+            style={{
+                width: '200px',
+                height: '200px',
+                border: '2px solid white',
+                cursor: 'pointer',
+                borderRadius: "5px",
+                ...selectionStyle
+            }}
+         
+            onClick={() => applyColor(setSelectionStyle)}>
+                <p style={{color: "white", fontWeight: 600, fontSize: 22, textAlign: "center"}}>Selection</p>
+        </div>
+    );
+}
+
+export default Selection;
 
